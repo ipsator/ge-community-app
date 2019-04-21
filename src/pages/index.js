fa137e7ed20450c3ea8035ceacac45f03983eb99
Abhinav Kushwaha <akush2007@gmail.com>
2019-04-21 16:30:43 +0530

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,9 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Home" keywords={[`ge`, `application`, `community`]} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new GE Community App site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
