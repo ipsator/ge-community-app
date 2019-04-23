20dbe4af7e6da0510b9ecc6717259c6dd8053898
Abhinav Kushwaha <akush2007@gmail.com>
2019-04-23 16:10:35 +0530

    test

--- src/pages/module-1/topic-1.js
+++ src/pages/module-1/topic-1.js
@@ -7,7 +7,6 @@ import SEO from "../../components/seo"
 const ThirdPage = () => (
   <Layout>
     <SEO title="मॉड्यूल 1 - Topic 1" />
-    <h1>Hi from the second page</h1>
     <p>Welcome to Module 1 - Topic 1</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
