cbb94f2a6bd8e527e293e2d7fb05ffa15b99455e
Abhinav Kushwaha <akush2007@gmail.com>
2019-05-01 10:17:08 +0530

    tags and images

--- src/firebase-upload/static-upload.js
+++ src/firebase-upload/static-upload.js
@@ -1,6 +1,8 @@
 const fs = require("fs")
 const path = require("path")
 
+const created = 1556680000000
+
 const saveData = (data, filename) => {
   fs.writeFileSync(path.join(__dirname, filename), JSON.stringify(data, undefined, 2), "utf8")
 }
@@ -17,12 +19,11 @@ const readCategories = () => {
   const categoriesData = categoryInfo.map(info => ({
     active: true,
     cover: info.cover || "https://i.imgur.com/xs8D9wZ.png",
-    created: Date.now(),
+    created: created,
     updated: Date.now(),
     ...info,
   }))
 
-  saveData(categoriesData, "../../static/data/categories.json")
   const topicData = []
   const articleData = []
   categoriesData.forEach((category, i) => {
@@ -36,15 +37,39 @@ const readCategories = () => {
         active: true,
         category: category.id,
         cover: topicDetails.cover || "https://i.imgur.com/xs8D9wZ.png",
-        created: Date.now(),
+        created: created,
         name: topicDetails.name,
         updated: Date.now(),
       })
       articleData.push(...getArticles(topicDetails, topicId))
     }
   })
+  const tags = []
+  const images = []
+  categoriesData.forEach(category => {
+    images.push(category.cover)
+  })
+  topicData.forEach(topic => {
+    images.push(topic.cover)
+  })
+  articleData.forEach(article => {
+    images.push(article.cover)
+    tags.push(...article.tags)
+    article.data.forEach(data => {
+      if (data.type === "image") {
+        data.content.forEach(img => {
+          if (img.indexOf("http") === 0) {
+            images.push(img)
+          }
+        })
+      }
+    })
+  })
+  saveData(categoriesData, "../../static/data/categories.json")
   saveData(topicData, "../../static/data/topics.json")
   saveData(articleData, "../../static/data/articles.json")
+  saveData([...new Set(tags)], "../../static/data/tags.json")
+  saveData([...new Set(images)], "../../static/data/images.json")
 }
 
 const getArticles = (topicInfo, topicId) => {
@@ -52,7 +77,7 @@ const getArticles = (topicInfo, topicId) => {
     active: true,
     topic: topicId,
     cover: slide.cover || "https://i.imgur.com/xs8D9wZ.png",
-    created: Date.now(),
+    created: created,
     name: slide.name,
     updated: Date.now(),
     data: slide.data,
