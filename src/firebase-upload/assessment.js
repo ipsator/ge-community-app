8c2dee0a80d0b041a61132ff49025b842ae48fce
Abhinav Kushwaha <akush2007@gmail.com>
2019-05-06 13:03:02 +0530

    assessment

--- src/firebase-upload/assessment.js
+++ src/firebase-upload/assessment.js
@@ -1,22 +1,7 @@
-const { GetDb } = require("./init-firebase")
+const { uploadData } = require("./init-firebase")
 const fs = require("fs")
 const path = require("path")
 // Add a new document in collection "cities"
-
-function uploadData(collectionName, data) {
-  let db = GetDb()
-  let id = db
-    .collection(collectionName)
-    .add(data)
-    .then(function(docRef) {
-      console.log("Document successfully written!", docRef.id)
-      return docRef.id
-    })
-    .catch(function(error) {
-      console.error("Error writing document: ", error)
-    })
-  return id
-}
 
 const uploadAssessment = async () => {
   let rawdata = fs.readFileSync("static/data/assessment" + ".json")
@@ -29,26 +14,21 @@ const uploadAssessment = async () => {
     questions: [],
     updated: Date.now(),
   }
-  question = {
-    active: true,
-    answer: 0,
-    created: String,
-    updated: String,
-    options: [],
-    question: String,
-  }
   for (let j = 0; j < assessmentData.questions.length; j++) {
+    let question = {
+      active: true,
+      answer: assessmentData.questions[j].answer,
+      created: Date.now(),
+      updated: Date.now(),
+      options: assessmentData.questions[j].options,
+      question: assessmentData.questions[j].question,
+    }
     console.log(assessmentData.questions[j])
-    question.answer = assessmentData.questions[j].answer
-    question.created = Date.now()
-    question.updated = Date.now()
-    question.options = assessmentData.questions[j].options
-    question.question = assessmentData.questions[j].question
     console.log("assessment.questions", question)
     let quesId = await uploadData("questions", question)
     assessment.questions.push(quesId)
   }
-  await uploadData("assessments", assessment)
+  await uploadData("assessments", assessment, "ge-meri-sakhi")
 }
 
 uploadAssessment()
